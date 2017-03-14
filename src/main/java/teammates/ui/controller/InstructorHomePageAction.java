788245a3b1a6bb3f7da77dee2a9da5f6b3ee88dc
JoshuaClark311 <Clarkjd@g.cofc.edu>
2017-03-14 10:28:10 -0400

    Remove unused parameter in InstructorHomeAjaxCoursePageData.java

--- src/main/java/teammates/ui/controller/InstructorHomePageAction.java
+++ src/main/java/teammates/ui/controller/InstructorHomePageAction.java
@@ -48,10 +48,10 @@ public class InstructorHomePageAction extends Action {
                 logic.getFeedbackResponseCommentsForSendingState(courseToLoad, CommentSendingState.PENDING)
                      .size();
         int pendingCommentsCount = commentsForSendingStateCount + feedbackResponseCommentsForSendingStateCount;
-        List<String> sectionNames = logic.getSectionNamesForCourse(course.course.getId());
+        
 
         InstructorHomeCourseAjaxPageData data = new InstructorHomeCourseAjaxPageData(account);
-        data.init(index, course, instructor, pendingCommentsCount, sectionNames);
+        data.init(index, course, instructor, pendingCommentsCount);
 
         statusToAdmin = "instructorHome Course Load:<br>" + courseToLoad;
 
